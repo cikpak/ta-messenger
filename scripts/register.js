8377cefc1de4ebb14f626d9528914af80677195c
cikpak <simion.gorgos@gmail.com>
2025-09-04 15:00:42 +0300

    Password validator

--- scripts/register.js
+++ scripts/register.js
@@ -1,18 +1,52 @@
+const showErrors = (errors) => {
+    errors.forEach(errors => {
+        Toastify({
+            text: errors,
+            className: "error-toast"
+        }).showToast();
+    })
+}
+
 const REGISTER_STEPS = {
-    accountInformation: {
+    accountInformation : {
         title: "Account information",
-        fields: ['name', 'city', 'country']
+        fields: ['name', 'city', 'country'],
     },
-    email: {
-        title: 'Email',
+    email : {
+        title: "Email",
         fields: ['email']
     },
     password: {
         title: "Password",
-        fields: ['password', 'confirmPassword']
+        fields: ['password', 'confirmPassword'],
+        validator: ({password, confirmPassword}) => {
+            const minLength = 6;
+            const hasUppercase = /[A-Z]/.test(password);
+            const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>_\-\\/~`+=]/.test(password);
+
+            const errors = [];
+
+            if (password.length < minLength) {
+                errors.push("Password should be at least 6 characters long.");
+            }
+
+            if (!hasUppercase) {
+                errors.push("Password should contain at least one uppercase letter.");
+            }
+
+            if (!hasSpecialChar) {
+                errors.push("Password should contain at least one special character.");
+            }
+
+            if(confirmPassword !== password) {
+                errors.push("Confirm password doesn't match with the password.");
+            }
+
+            return errors
+        }
     },
     registrationSuccess: {
-        title: "Registration Success"
+        title: "Registration complete!"
     }
 }
 
@@ -45,11 +79,21 @@ const moveToTheNextStep = () => {
 
 const nextStepHandler = (event) => {
     event.preventDefault();
+    const activeStepData = REGISTER_STEPS[activeStep];
 
     const stepData = {};
-    REGISTER_STEPS[activeStep].fields.forEach(field => {
+    activeStepData.fields.forEach(field => {
         stepData[field] = event.target.elements[field].value 
     })
+
+    if(activeStepData?.validator) {
+        const errors = activeStepData.validator(stepData);
+
+        if(errors.length > 0) {
+            showErrors(errors);
+            return
+        }
+    }
 
     registerPayload = {
         ...registerPayload,
